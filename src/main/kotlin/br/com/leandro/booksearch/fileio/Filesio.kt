a94716e691320c9294465d763d05d9556f7f78d8
Leandro Ferreira <lehen01@gmail.com>
2020-04-27 00:27:00 +0200

    files io package

---
+++ src/main/kotlin/br/com/leandro/booksearch/fileio/Filesio.kt
@@ -0,0 +1,23 @@
+package br.com.leandro.booksearch.fileio
+
+import br.com.leandro.booksearch.book.domain.Book
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import java.io.File
+
+private fun writeToFile(json: String, fileName: String) {
+    File(fileName).bufferedWriter().use { out -> out.write(json) }
+}
+
+fun writeBookToFile(book: Book) {
+    writeToFile(getGson().toJson(book), book.title)
+}
+
+fun readBookFromFile(fileName: String): Book =
+    File(fileName).bufferedReader().use { reader ->
+        getGson().fromJson(reader, Book::class.java)
+    }
+
+private fun getGson(): Gson = GsonBuilder()
+    .setPrettyPrinting()
+    .create()
